31916819a7e78001905d259c482e0769dd743693
Francis-Adjei <francis.adjei@amalitech.org>
2021-07-28 12:56:01 +0000

    Dog Years

---
+++ main.js
@@ -0,0 +1,25 @@
+// declaring and varible and assigning it
+let myAge = 34
+
+// declaring and varible and assigning it
+let earlyYears = 2
+earlyYears *= 10.5;
+earlyYears = earlyYears
+// declaring and varible and assigning it
+myAge -= 2
+
+// declaring and varible and assigning it to myAge
+let laterYears = myAge
+
+// multipling laterYears by 4
+laterYears *= 4;
+console.log(earlyYears)
+console.log(laterYears)
+
+myAgeInDogYears = laterYears + earlyYears
+console.log(myAgeInDogYears)
+
+myName = 'FRANCIS'.toLowerCase()
+console.log(myName)
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
